ca3950a50d4932d82fd4cf2156d1fc7b8b350c28
AnujinLhagvasuren <144761548+AnujinLhagvasuren@users.noreply.github.com>
2023-11-06 13:43:04 +0800

    Add files via upload

---
+++ pset5.py
@@ -0,0 +1,16 @@
+import streamlit as st
+st.slider
+
+feeling = st.slider('How are you today?', 1, 10, 5)
+
+if feeling >=6:
+    mood = "great"
+elif feeling <=4:
+    mood = "bad"
+elif feeling == 5:
+    mood = "ok"
+    
+st.write("I feel",mood)
+
+
+
