c3289450bdc986d3b92be75f4e66fe84607affa5
AnujinLhagvasuren <144761548+AnujinLhagvasuren@users.noreply.github.com>
2023-11-07 08:26:50 +0800

    Update pset5.py

--- pset5.py
+++ pset5.py
@@ -15,5 +15,55 @@ st.write("I feel", mood)
 image_path = f"images/{mood}.jpg"
 st.image(Image.open(image_path))
 
+if mood in mood_images:
+    image_path = mood_images[mood]
+    st.image(Image.open(image_path), caption=f'Your {mood} mood!')
+else:
+    st.error("Image not found for this mood")
+
+
+if st.button("Refresh"):
+    st.experimental_rerun()
+
+
+if mood in mood_images:
+    image_path = mood_images[mood]
+    if os.path.exists(image_path):
+        st.image(Image.open(image_path), caption=f'Your {mood} mood!')
+    else:
+        st.warning(f"Image not found for {mood} mood. Showing a placeholder image.")
+        st.image("images/placeholder.jpg")
+else:
+    st.error("Image not found for this mood")
+
+feeling = st.slider('How are you today?', 1, 10, 5, format="%d/10")
+
+mood_descriptions = {
+    "great": "Feeling fantastic today!",
+    "ok": "Having an average day.",
+    "bad": "Not feeling my best."
+}
+
+# ...
+
+st.write("I feel", mood)
+if mood in mood_descriptions:
+    st.write(mood_descriptions[mood])
+    
+mood_images = {
+    "great": "images/great.jpg",
+    "ok": "images/ok.jpg",
+    "bad": "images/bad.jpg",
+    "awful": "images/awful.jpg",
+    # Add more moods and corresponding image paths here
+}
+
+st.title('How do you feel today')
+st.markdown("<p style='text-align: center;'>Use the slider to indicate your mood</p>", unsafe_allow_html=True)
+st.sidebar.markdown("Welcome to the Mood Analyzer")
+st.markdown("<style>body { background-color: #f2f2f2; }</style>", unsafe_allow_html=True)
+
+
+
 
 
